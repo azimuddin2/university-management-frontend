817c359e2a0feb072174a812c20d0c2f5b60da52
azimuddin2 <mohammadazimuddin274@gmail.com>
2025-01-11 22:09:56 +0600

    added sidebarItemsGenerator and routesGenerators functionlity task

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,34 +1,10 @@
 import { Layout, Menu } from "antd";
 import logo from '../../assets/icons/ph_logo.svg';
 import { Outlet } from "react-router-dom";
-import { adminSidebarItems } from "../../routes/admin.routes";
+import { adminPaths } from "../../routes/admin.routes";
+import { sidebarItemsGenerator } from "../../utils/sidebarItemsGenerator";
 
 const { Header, Content, Footer, Sider } = Layout;
-
-// const items: MenuProps["items"] = [
-//     {
-//         key: 'Dashboard',
-//         label: <NavLink to="/admin/dashboard">Dashboard</NavLink>
-//     },
-//     {
-//         key: 'User Management',
-//         label: 'User Management',
-//         children: [
-//             {
-//                 key: 'Create Admin',
-//                 label: <NavLink to="/admin/create-admin">Create Admin</NavLink>
-//             },
-//             {
-//                 key: 'Create Faculty',
-//                 label: <NavLink to="/admin/create-faculty">Create Faculty</NavLink>
-//             },
-//             {
-//                 key: 'Create Student',
-//                 label: <NavLink to="/admin/create-student">Create Student</NavLink>
-//             },
-//         ],
-//     },
-// ];
 
 const MainLayout = () => {
     return (
@@ -67,7 +43,7 @@ const MainLayout = () => {
                     theme="dark"
                     mode="inline"
                     defaultSelectedKeys={['4']}
-                    items={adminSidebarItems}
+                    items={sidebarItemsGenerator(adminPaths, "admin")}
                 />
             </Sider>
             <Layout>
