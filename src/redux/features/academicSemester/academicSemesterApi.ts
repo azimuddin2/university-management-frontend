9110db437e38445e26ff38957d765cf78046874a
azimuddin2 <mohammadazimuddin274@gmail.com>
2025-01-19 13:59:32 +0600

    auth task complete

--- src/redux/features/academicSemester/academicSemesterApi.ts
+++ src/redux/features/academicSemester/academicSemesterApi.ts
@@ -1,14 +1,14 @@
-import { baseApi } from "../../api/baseApi";
+import { baseApi } from '../../api/baseApi';
 
 const academicSemesterApi = baseApi.injectEndpoints({
-    endpoints: (builder) => ({
-        getAllSemesters: builder.query({
-            query: () => ({
-                url: '/academic-semesters',
-                method: 'GET',
-            }),
-        }),
+  endpoints: (builder) => ({
+    getAllSemesters: builder.query({
+      query: () => ({
+        url: '/academic-semesters',
+        method: 'GET',
+      }),
     }),
+  }),
 });
 
 export const { useGetAllSemestersQuery } = academicSemesterApi;
