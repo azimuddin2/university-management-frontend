6c70bd6f0a6f2d5d32b6bd504b9d09b0f086df3e
azimuddin2 <mohammadazimuddin274@gmail.com>
2025-01-20 12:39:53 +0600

    added PHSelect component

--- src/components/form/PHInput.tsx
+++ src/components/form/PHInput.tsx
@@ -1,4 +1,4 @@
-import { Input } from "antd";
+import { Form, Input } from "antd";
 import { Controller } from "react-hook-form";
 
 type TInputProps = {
@@ -9,13 +9,14 @@ type TInputProps = {
 
 const PHInput = ({ type, name, label }: TInputProps) => {
     return (
-        <div style={{ marginBottom: "12px" }}>
-            {label ? label : null}
-            <Controller
-                name={name}
-                render={({ field }) => <Input {...field} type={type} id={name} required />}
-            />
-        </div>
+        <Controller
+            name={name}
+            render={({ field }) => (
+                <Form.Item style={{marginBottom: "12px"}} label={label}>
+                    <Input {...field} type={type} id={name} required />
+                </Form.Item>
+            )}
+        />
     );
 };
 
--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -1,8 +1,44 @@
+import { Button, Col, Flex } from "antd";
+import PHForm from "../../../components/form/PHForm";
+import PHSelect from "../../../components/form/PHSelect";
+import { FieldValues, SubmitHandler } from "react-hook-form";
+
+const nameOptions = [
+    {
+        value: "Autumn",
+        label: "Autumn",
+    },
+    {
+        value: "Summar",
+        label: "Summar",
+    },
+    {
+        value: "Fall",
+        label: "Fall",
+    },
+];
 
 const CreateAcademicSemester = () => {
+
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
+        const semesterData = {
+            name: 'Something',
+            code: 'something',
+        };
+        console.log(semesterData);
+    };
+
     return (
         <div>
             <h2>Create Academic Semester</h2>
+            <Flex justify="center" align="center">
+                <Col sm={18} lg={8}>
+                    <PHForm onSubmit={onSubmit}>
+                        <PHSelect label="Name" name="name" options={nameOptions} />
+                        <Button type="primary" htmlType="submit">Submit</Button>
+                    </PHForm>
+                </Col>
+            </Flex>
         </div>
     );
 };
